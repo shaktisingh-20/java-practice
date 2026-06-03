bb314c70fbae93169eedb4381e01fb34ec13dbb0
Shakti Singh <shakti33123singh@gmail.com>
2026-06-03 11:42:12 +0530

    deleted all the files to ahave a structured flow

--- main.java
+++ main.java
@@ -1,13 +1,5 @@
-import java.util.*;
-public class main{
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-
-        if(age>18){
-            System.out.println("Adult");
-        }else{
-            System.out.println("not adult");
-        }
+class Main{
+    public static void main(String[] args){
+        System.out.println(args[0]);
     }
 }
