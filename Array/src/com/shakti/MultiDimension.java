444a77792c8145636c9824eae7526b80b3e77d4c
Shakti Singh <shakti33123singh@gmail.com>
2026-07-03 21:35:24 +0530

    multidimension arrays and their syntax

---
+++ Array/src/com/shakti/MultiDimension.java
@@ -0,0 +1,18 @@
+package com.shakti;
+
+public class MultiDimension {
+    public static void main(String[] args) {
+        /*
+        1 2 3
+        4 5 6
+        7 8 9
+        */
+
+        int [] [] arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+        };
+        System.out.println(arr[1][0]);
+    }
+}
