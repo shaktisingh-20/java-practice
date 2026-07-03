1d5ede7c2c2f59a7e011381e5e98c5b3711d3344
Shakti Singh <shakti33123singh@gmail.com>
2026-07-03 18:36:08 +0530

    learned about internal working of object and create program of input from for loop in array

---
+++ Array/src/com/shakti/Input.java
@@ -0,0 +1,16 @@
+package com.shakti;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+        //input using for loops
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
--- Array/src/com/shakti/Main.java
+++ Array/src/com/shakti/Main.java
@@ -8,5 +8,9 @@ public class Main {
             //or
         int [] arr = {1,2,4,65,457,75,4};
         System.out.println(arr[3]);
+
+        String [] arr2 = new String [5];
+        System.out.println(arr2[0]);
+
     }
 }
