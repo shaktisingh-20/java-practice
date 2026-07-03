b293bf5a5c21ee61bc83f5ba2a2b5bf072a722c7
Shakti Singh <shakti33123singh@gmail.com>
2026-07-03 21:22:54 +0530

    built a program showing passing by reference in function in array

--- Array/src/com/shakti/PassininFunctions.java
+++ Array/src/com/shakti/PassininFunctions.java
@@ -1,4 +1,15 @@
 package com.shakti;
 
+import java.util.Arrays;
+
 public class PassininFunctions {
+    public static void main(String[] args) {
+        int [] arr = {12,34,56,78,89};
+        System.out.println(Arrays.toString(arr));
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void change(int[] arr2){
+        arr2 [3] = 98;
+    }
 }
