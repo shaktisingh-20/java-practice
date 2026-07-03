59287b57704b8e9751eeb0875c624d3178984a86
Shakti Singh <shakti33123singh@gmail.com>
2026-07-03 17:50:28 +0530

    learning array why and syntax

---
+++ Array/src/com/shakti/Main.java
@@ -0,0 +1,12 @@
+package com.shakti;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] ros = new int[5];
+        System.out.println(ros[3]);
+
+            //or
+        int [] arr = {1,2,4,65,457,75,4};
+        System.out.println(arr[3]);
+    }
+}
--- methods/src/com/shakti/functionOverloading.java
+++ methods/src/com/shakti/functionOverloading.java
@@ -2,16 +2,19 @@ package com.shakti;
 
 public class functionOverloading {
     static void main(String[] args) {
-
+        int sum1 = sum(2,4);
+        System.out.println(sum1);
+        int sum2 = sums(2,5,7);
+        System.out.println(sum2);
     }
-    static void fun(int a ){
-
+    static int sum(int a,int b ){
+        return a+b;
     };
 
-    static void fun(int k,int i){
-
+    static int sums(int a,int b, int c){
+        return a+b+c;
     }
     static void fun(int j, String k){
-        
+
     }
 }
