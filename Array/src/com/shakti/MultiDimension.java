d3fdab297dc833e2a0ed482e6ea58671cc9bcddc
Shakti Singh <shakti33123singh@gmail.com>
2026-07-03 22:07:19 +0530

    input in 2d array and its internal working

--- Array/src/com/shakti/MultiDimension.java
+++ Array/src/com/shakti/MultiDimension.java
@@ -1,5 +1,8 @@
 package com.shakti;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MultiDimension {
     public static void main(String[] args) {
         /*
@@ -7,12 +10,22 @@ public class MultiDimension {
         4 5 6
         7 8 9
         */
-        int [] [] arr2 = new int [3][];
-        int [] [] arr = {
-                {1,2,3},
-                {4,5},
-                {7,8,9,12},
-        };
-        System.out.println(arr[1][0]);
+//        int [] [] arr2 = new int [3][];
+//        int [] [] arr = {
+//                {1,2,3},
+//                {4,5},
+//                {7,8,9,12},
+//        };
+
+        int [][] arr = new int [3][2];
+        Scanner in = new Scanner(System.in);
+
+        for (int row= 0; row< arr.length; row++){
+            //for each column in every row
+            for (int col = 0;col < arr[row].length; col++){
+                arr[row][col] = in.nextInt();
+            };
+        }
+        System.out.println(Arrays.deepToString(arr));
     }
 }
