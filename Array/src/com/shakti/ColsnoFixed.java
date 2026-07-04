fb8c4e8ba40fc32ee339dd281ccdd49786eb0fc1
Shakti Singh <shakti33123singh@gmail.com>
2026-07-04 16:10:49 +0530

    output in 2d arrays with col number fixed

---
+++ Array/src/com/shakti/ColsnoFixed.java
@@ -0,0 +1,17 @@
+package com.shakti;
+
+public class ColsnoFixed {
+    public static void main(String[] args) {
+        int [] [] arr ={
+                {1,2,3,4},
+                {5,6},
+                {7,8,9}
+        };
+        for (int rows = 0; rows <arr.length; rows++){
+            for (int col = 0; col < arr[rows].length; col++) {
+                System.out.print(arr[rows][col]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
