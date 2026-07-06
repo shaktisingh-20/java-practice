2ed3c0e9f5aeacbc2e61ec987345959abcf33fff
Shakti Singh <shakti33123singh@gmail.com>
2026-07-06 10:47:54 +0530

    understood linear search basics

---
+++ LinearSearch/src/com/shakti/Main.java
@@ -0,0 +1,25 @@
+package com.shakti;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {23, 45, 67, 43, 67, -43, 12, -89, -12};
+        int target = 43;
+        int ans = linearSearch(nums, target);
+        System.out.println(ans);
+    }
+
+
+    static int linearSearch(int[]arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        for (int index = 0; index < arr.length; index++) {
+            int element = arr[index];
+            if (element == target) {
+                return index;
+            }
+        }
+        return -1;
+    }
+}
