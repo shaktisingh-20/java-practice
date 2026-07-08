45317a5426b359e139aeec92ffd1bd620c4123ca
Shakti Singh <shakti33123singh@gmail.com>
2026-07-08 17:10:20 +0530

    made program to search elemet in a string

--- LinearSearch/src/com/shakti/searchInStrings.java
+++ LinearSearch/src/com/shakti/searchInStrings.java
@@ -1,4 +1,37 @@
 package com.shakti;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class searchInStrings {
+    public static void main(String[] args) {
+        String name = "Shakti";
+        char target = 'l';
+        System.out.println(search(name,target));
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+
+    static boolean search(String name, char target){
+        if (name.length() ==0){
+            return false;
+        }
+        for (int index = 0; index < name.length(); index++) {
+            if(target == name.charAt(index)){
+                return true;
+            }
+        }
+        return false;
+    }
+    static boolean search2(String name, char target){
+        if (name.length() == 0){
+            return false;
+        }
+        // using foreach loop
+        for(char ch :name.toCharArray()){
+            if(ch == target){
+                return true;
+            }
+        }
+        return false;
+    }
 }
