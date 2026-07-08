84ed80de0150992bcce1213958a1cf35013c003f
Shakti Singh <shakti33123singh@gmail.com>
2026-07-08 16:40:29 +0530

    linear search first program

---
+++ LinearSearch/src/com/shakti/firstLinearSearch.java
@@ -0,0 +1,52 @@
+package com.shakti;
+
+public class firstLinearSearch {
+    public static void main(String[] args) {
+        int [] arr = {21,43,45,18,36,89,-1};
+        int target = 18;
+        int ans = search(arr, target);
+        System.out.println(ans);
+    }
+    static int search(int[]nums, int target){
+        if(nums.length==0){
+            return -1;
+        }
+
+        for (int index = 0; index < nums.length; index++) {
+            int element = nums[index];
+            if(element == target){
+                return index;
+            }
+        }
+        return -1;
+    }
+    static int search2(int[]nums, int target){
+        if(nums.length==0){
+            return -1;
+        }
+
+        for (int index = 0; index < nums.length; index++) {
+            int element = nums[index];
+            if(element == target){
+                return element;
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+
+
+    //gives true or false
+    static boolean search3(int[]nums, int target){
+        if(nums.length==0){
+            return false;
+        }
+
+        for (int index = 0; index < nums.length; index++) {
+            int element = nums[index];
+            if(element == target){
+                return true;
+            }
+        }
+        return false;
+    }
+}
