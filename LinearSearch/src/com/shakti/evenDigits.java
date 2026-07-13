6a95fcc29412ee4772877ba816f46c8ce638f66a
Shakti Singh <shakti33123singh@gmail.com>
2026-07-13 15:47:53 +0530

    built program

--- LinearSearch/src/com/shakti/evenDigits.java
+++ LinearSearch/src/com/shakti/evenDigits.java
@@ -1,4 +1,29 @@
 package com.shakti;
 
 public class evenDigits {
+    public static void main(String[] args) {
+        int[] nums = {12,345,2,6,7896};
+    }
+    static int findNumbers(int[] nums) {
+        int count = 0;
+        for(int num : nums) {
+            if (even(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    static boolean even(int num) {
+        int numberOfDigits = digits(num);
+        return numberOfDigits % 2 == 0;
+    }
+
+    static int digits(int num){
+        int count = 0;
+        while(num>0){
+            count ++;
+            num= num/10;
+        }
+        return count;
+    }
 }
