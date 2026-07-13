9958cdb3f704991c41ed50e9a82334d04679ec33
Shakti Singh <shakti33123singh@gmail.com>
2026-07-13 11:53:17 +0530

    built a find min value in array program

--- LinearSearch/src/com/shakti/practiceMinVal.java
+++ LinearSearch/src/com/shakti/practiceMinVal.java
@@ -1,4 +1,23 @@
 package com.shakti;
 
 public class practiceMinVal {
+    public static void main(String[] args) {
+        int [] arr = {12,34,3,45,576,};
+        int minVal = findMinVal(arr);
+        System.out.println(minVal);
+    }
+
+    static int findMinVal(int[]num){
+        int ans = num[0];
+        if (num.length == 0) {
+            return -1;
+        }
+        for (int index = 0; index < num.length; index++) {
+
+            if(num[index]<ans){
+                ans = num[index];
+            }
+        }
+        return ans;
+    }
 }
