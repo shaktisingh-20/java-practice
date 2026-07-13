6a14f367fcc1b29b058702f35563e3c4716fe2d9
Shakti Singh <shakti33123singh@gmail.com>
2026-07-13 11:52:12 +0530

    made a program of linear sarch in range

--- LinearSearch/src/com/shakti/practice2.java
+++ LinearSearch/src/com/shakti/practice2.java
@@ -1,4 +1,23 @@
 package com.shakti;
-
+//search in range
 public class practice2 {
+    public static void main(String[] args) {
+        int [] arr = {12,34,56,76,34,21};
+        int target = 56;
+        int ans = linearRange(arr,target,3,5);
+        System.out.println(ans);
+    }
+
+    static int linearRange (int [] nums, int target, int start, int end){
+        if (nums.length == 0) {
+            return -1;
+        }
+        for (int index = start; index <end ; index++) {
+            int element = nums[index];
+            if (element == target) {
+                return index;
+            }
+        }
+        return -1;
+    }
 }
